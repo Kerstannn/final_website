98a4b499e467f3bdf3a821f0b4da9381ed1cc943
π-non <densheers@gmail.com>
2026-06-17 16:41:05 +0000

    Made pages functional

--- assignment3/portalapp/models.py
+++ assignment3/portalapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Registration(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    interest = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
--- assignment3/portalapp/views.py
+++ assignment3/portalapp/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from .forms import RegistrationForm
 
-# Create your views here.
+def home(request):
+    return render(request, 'portal/home.html')
+
+def about(request):
+    return render(request, 'portal/about.html')
+
+def register(request):
+    
+    message = ""
+
+    if request.method == "POST":
+        form = RegistrationForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            message = "Registration submitted successfully!"
+            form = RegistrationForm()
+
+    else:
+        form = RegistrationForm()
+
+    return render(request,
+                  'portal/form.html',
+                  {'form': form,
+                   'message': message})
